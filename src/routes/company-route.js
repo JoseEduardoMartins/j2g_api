0786b2c2a20f0d2b187aa5d1347afdf4e0aad36d
José Eduardo Martins <49596282+JoseEduardoMartins@users.noreply.github.com>
2022-04-21 18:08:06 -0300

    Update company-route.js

--- src/routes/company-route.js
+++ src/routes/company-route.js
@@ -1,15 +1,15 @@
-// 'use strict';
-// //imports
-// const express = require('express');
-// const router = express.Router();
-// //controller
-// const controller = require('../controllers/customerUser-controller');
-// //ROUTES GET
-// router.get('/get', controller.get);
-// router.get('/getById', controller.getById);
-// //ROUTES POST
-// router.post('/set', controller.set);
-// //ROUTES PUT
-// router.put('/update', controller.update);
-// //export
-// module.exports = router;
+'use strict';
+//imports
+const express = require('express');
+const router = express.Router();
+//controller
+const controller = require('../controllers/customerUser-controller');
+//ROUTES GET
+router.get('/get', controller.get);
+router.get('/getById', controller.getById);
+//ROUTES POST
+router.post('/set', controller.set);
+//ROUTES PUT
+router.put('/update', controller.update);
+//export
+module.exports = router;
